4147302b4bdc47671f2918b71e816b0293c40df0
Alysson <alyssonredes32@gmail.com>
2025-01-18 13:29:19 -0300

    feature/adicionando validação de form + mensagens dinamicas com jquery

--- main.js
+++ main.js
@@ -8,4 +8,32 @@ $(document).ready(function(){
     $('.menu-hamburguer').click(function(){
         $('nav').slideToggle('slow');
     })
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(xx) xxxxx-xxxx'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {required: true},
+            email: {required: true, email: true},
+            telefone: {required: true, minlength: 15},
+            mensagem: {required: true},
+            veiculoInteresse: { required: false},
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            telefone: 'Por favor verifique o telefone'
+        },
+        /* interceptar o evento de submit */
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos inválidos!`);
+            }
+        }
+    })
 });
