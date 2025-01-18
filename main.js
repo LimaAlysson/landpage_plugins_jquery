bfd46d67645a92f075309c62965f79215decd24f
Alysson <alyssonredes32@gmail.com>
2025-01-18 11:17:28 -0300

    feature/adicionando seções e estilizando

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     $('#carousel-images').slick({
         autoplay: true,
+        arrows: false, // remove as setas
+        dots: false, // remove os pontos de navegação
     });
 
+    $('.menu-hamburguer').click(function(){
+        $('nav').slideToggle('slow');
+    })
 });
